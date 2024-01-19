013a7e3a9b6d222dd2c6a045fb370a2faa3fdbed
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-19 06:21:40 -0600

    Outline a plan

--- project.js
+++ project.js
@@ -1,3 +1,7 @@
-//Despot some money
+// Despot some money
 // Determine number of lines to bet on
-// Collect
+// Collect a bet amount
+// Spin the slot machine
+// Check if the user won
+// give the user their winnings
+// play again
