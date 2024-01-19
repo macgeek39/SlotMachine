8585b85330f6dd4d67371d7b001d1d3e3b2b1ecd
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-19 06:39:17 -0600

    Created Despot money

--- project.js
+++ project.js
@@ -5,3 +5,19 @@
 // Check if the user won
 // give the user their winnings
 // play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+      const depositAmount = prompt("Enter a deposit amount: ");
+      const numberDepositAmount = parseFloat(depositAmount);
+  
+      if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, try again.");
+      } else {
+        return numberDepositAmount;
+      }
+    }
+  };
+deposit();
